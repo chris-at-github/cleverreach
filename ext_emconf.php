96ea4c5e535e57c473c49bad6b1cb25a732eadf7
Christian Pschorr <c.pschorr@datamints.com>
2022-01-12 23:37:58 +0100

    Abhängigkeit auf Powermail hinterlegt

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '9.5.0-10.4.999',
             'php' => '7.0.0-7.4.99',
+            'powermail' => '8.4.1-8.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
